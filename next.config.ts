4c774ab9586dd2d2fb8dd5b904d9d12a443d7f8b
mojcamarin <marin.mojca14@gmail.com>
2026-03-13 21:39:53 +0100

    Added coming soon page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // izklopi Turbopack
+  turbopack: {
+    // prazna konfiguracija = Turbopack je izklopljen
+  },
+
+  // omogoči Webpack
+  webpack: (config) => {
+    return config;
+  },
 };
 
 export default nextConfig;
