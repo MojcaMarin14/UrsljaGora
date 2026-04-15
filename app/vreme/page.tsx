c0f1ddb618afa580f05edef62c85f295b538ceb2
Gaj Korošec <gaj.korosec@student.um.si>
2026-04-15 13:21:00 +0200

    fix: fixed vreme page performance
    vreme is now cached for 30min so it doesnt fetch on every request reducing api calls

--- app/vreme/page.tsx
+++ app/vreme/page.tsx
@@ -1,10 +1,17 @@
 import WeatherClient from "../components/WeatherClient";
 
+export const revalidate = 1800;
+
 async function getWeather() {
   const res = await fetch(
     "https://api.open-meteo.com/v1/forecast?latitude=46.48&longitude=14.966&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,snowfall_sum,precipitation_sum&timezone=Europe%2FLjubljana",
-    { cache: "no-store" }
+    { next: { revalidate } }
   );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch weather data");
+  }
+
   return res.json();
 }
 
